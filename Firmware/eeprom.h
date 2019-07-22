06ad91f02790e0bdc83461800ef66f91f510690b
Laurentiu <mionut@mail.com>
2019-07-23 00:53:25 +0300

    EEPROM_LAST_ITEM

--- Firmware/eeprom.h
+++ Firmware/eeprom.h
@@ -169,7 +169,7 @@
 #define EEPROM_UVLO_LA_K (EEPROM_NOZZLE_DIAMETER_uM-4) // float
 
 //This is supposed to point to last item to allow EEPROM overrun check. Please update when adding new items.
-#define EEPROM_LAST_ITEM EEPROM_NOZZLE_DIAMETER_uM
+#define EEPROM_LAST_ITEM EEPROM_UVLO_LA_K
 
 // !!!!!
 // !!!!! this is end of EEPROM section ... all updates MUST BE inserted before this mark !!!!!
