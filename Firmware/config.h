4f5c78f9e1a97cdc42b19ef581090d58cd54c337
Laurentiu <mionut@mail.com>
2019-07-23 03:02:23 +0300

    linux line endings

--- Firmware/config.h
+++ Firmware/config.h
@@ -46,7 +46,7 @@
 #include "Configuration_prusa.h"
 
 //LANG - Multi-language support
-#define LANG_MODE              0 // primary language only
+#define LANG_MODE              1 // primary language only
 //#define LANG_MODE              1 // sec. language support
 
 #define LANG_SIZE_RESERVED     0x2800 // reserved space for secondary language (10240 bytes)
